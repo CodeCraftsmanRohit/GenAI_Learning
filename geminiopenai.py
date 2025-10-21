9ac9b41eddbe6297c4b0d11f4bd58f42ab938a25
Rohit Kumar <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-10-21 18:12:55 +0000

    setting gemini api

---
+++ geminiopenai.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="AIzaSyCAAXF3aJD8ywcrMSnkDXLmsX185axXqlc",
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    reasoning_effort="low",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Explain to me how AI works"
+        }
+    ]
+)
+
+print(response.choices[0].message)
